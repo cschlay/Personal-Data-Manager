ae4b1966c18e1041ffb6694d43aacc2222523366
chlay <32131329+CHLay@users.noreply.github.com>
2018-05-27 08:41:18 +0300

    s

--- apps/budget/models.py
+++ apps/budget/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Revenue(models.Model):
     account_id = models.IntegerField()
     amount  = models.IntegerField()
-    comment = models.CharField(max_length = 50, null = True)
+    comment = models.CharField(max_length=50, null=True)
 
 
 class Account(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    account_name = models.CharField(max_length = 20, null = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=20, null=True)
