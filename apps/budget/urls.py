2ed351251e96c4b9b92bfdc0f2d1782a0be8dd16
chlay <32131329+CHLay@users.noreply.github.com>
2018-05-27 12:02:50 +0300

    Income-adding tool.

--- apps/budget/urls.py
+++ apps/budget/urls.py
@@ -6,4 +6,5 @@ app_name = 'budget'
 urlpatterns = [
     #path('', views.budget, name='budget'),
     #path('new-account/', views.new_account, name='new_account')
+    path('add-income/', views.add_revenue_record, name='add-income')
 ]
--- apps/budget/views.py
+++ apps/budget/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, redirect
 
-from apps.budget.models import Account
+from apps.budget.models import Revenue
 
 
 def budget(request):
-    return render(request, 'budget.html')
+    user = request.user
+
+    revenues = Revenue.objects.filter(user=user).order_by('date')
+
+    context = {'user': user, 'revenues': reversed(revenues)}
+
+    # Add the comma to currency.
+    for record in revenues:
+        a = str(record.amount)
+        record.amount = a[:len(a)-2] + ',' + a[len(a)-2:]
+
+    return render(request, 'budget.html', context)
 
 
-def new_account(request):
-    query = request.GET
-    name = query.__getitem__('name')
+def add_revenue_record(request):
+    """
+    Add new revenue record to database.
 
-    account = Account(user=request.user, account_name=name)
-    account.save()
+    :param request:
+    """
+    a = request.POST['amount']
+    a = a[:len(a)-3] + a[len(a)-2:]
 
-    return redirect('index')
+    record = Revenue(
+        user=request.user,
+        date=request.POST['date'],
+        amount=int(a),
+        comment=request.POST['comment'])
+    record.save()
+
+    return redirect('budget')
