be6fa928d5fce4ac99f0290dc0c1f6f00384accf
chi-hao.lay@outlook.com <development@cschlay.com>
2021-01-23 09:39:38 +0200

    Move some values to .env

--- pdatamana/settings.py
+++ pdatamana/settings.py
@@ -22,12 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('SECRET_KEY.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"] == "1"
 
 ALLOWED_HOSTS = []
 
